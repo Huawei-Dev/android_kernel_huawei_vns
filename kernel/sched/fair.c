fbadb2183ea286df123fa8542ce2ab0eeec20bda
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-07-21 02:05:24 -0700

    sched: support legacy mode better
    It should be possible to bypass all HMP scheduler changes at runtime
    by setting sysctl_sched_enable_hmp_task_placement and
    sysctl_sched_enable_power_aware to 0.  Fix various code paths to honor
    this requirement.
    
    Change-Id: I74254e68582b3f9f1b84661baf7dae14f981c025
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
    [joonwoop@codeaurora.org: fixed conflict in rt.c, p->nr_cpus_allowed ==
     1 is now moved in core.c]
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2815,6 +2815,9 @@ int sched_set_boost(int enable)
 	unsigned long flags;
 	int ret = 0;
 
+	if (!sysctl_sched_enable_hmp_task_placement)
+		return -EINVAL;
+
 	spin_lock_irqsave(&boost_lock, flags);
 
 	if (enable == 1) {
@@ -3120,6 +3123,9 @@ done:
 
 void inc_nr_big_small_task(struct rq *rq, struct task_struct *p)
 {
+	if (!sysctl_sched_enable_hmp_task_placement)
+		return;
+
 	if (is_big_task(p))
 		rq->nr_big_tasks++;
 	else if (is_small_task(p))
@@ -3128,6 +3134,9 @@ void inc_nr_big_small_task(struct rq *rq, struct task_struct *p)
 
 void dec_nr_big_small_task(struct rq *rq, struct task_struct *p)
 {
+	if (!sysctl_sched_enable_hmp_task_placement)
+		return;
+
 	if (is_big_task(p))
 		rq->nr_big_tasks--;
 	else if (is_small_task(p))
@@ -3193,7 +3202,7 @@ int sched_hmp_proc_update_handler(struct ctl_table *table, int write,
 	unsigned int old_val = *data;
 
 	ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
-	if (ret || !write)
+	if (ret || !write || !sysctl_sched_enable_hmp_task_placement)
 		return ret;
 
 	if ((sysctl_sched_downmigrate_pct > sysctl_sched_upmigrate_pct) ||
@@ -3316,7 +3325,8 @@ static inline int migration_needed(struct rq *rq, struct task_struct *p)
 {
 	int nice = task_nice(p);
 
-	if (is_small_task(p) || p->state != TASK_RUNNING)
+	if (is_small_task(p) || p->state != TASK_RUNNING ||
+			!sysctl_sched_enable_hmp_task_placement)
 		return 0;
 
 	/* Todo: cgroup-based control? */
@@ -3395,11 +3405,6 @@ static inline int find_new_hmp_ilb(int type)
 static inline int power_cost(struct task_struct *p, int cpu)
 {
 	return SCHED_CAPACITY_SCALE;
-}
-
-static unsigned int power_cost_at_freq(int cpu, unsigned int freq)
-{
-	return 1;
 }
 
 static inline int
@@ -3976,6 +3981,9 @@ add_to_scaled_stat(int cpu, struct sched_avg *sa, u64 delta)
 	u64 scaled_delta;
 	int sf;
 
+	if (!sysctl_sched_enable_hmp_task_placement)
+		return;
+
 	if (unlikely(cur_freq > max_possible_freq ||
 		     (cur_freq == max_freq &&
 		      max_freq < cpu_max_possible_freq)))
@@ -3990,6 +3998,9 @@ add_to_scaled_stat(int cpu, struct sched_avg *sa, u64 delta)
 
 static inline void decay_scaled_stat(struct sched_avg *sa, u64 periods)
 {
+	if (!sysctl_sched_enable_hmp_task_placement)
+		return;
+
 	sa->runnable_avg_sum_scaled =
 		decay_load(sa->runnable_avg_sum_scaled,
 			   periods);
@@ -9238,11 +9249,8 @@ out_balanced:
 	return NULL;
 }
 
-/*
- * find_busiest_queue - find the busiest runqueue among the cpus in group.
- */
 #ifdef CONFIG_SCHED_HMP
-static struct rq *find_busiest_queue(struct lb_env *env,
+static struct rq *find_busiest_queue_hmp(struct lb_env *env,
 				     struct sched_group *group)
 {
 	struct rq *busiest = NULL, *rq;
@@ -9263,13 +9271,26 @@ static struct rq *find_busiest_queue(struct lb_env *env,
 
 	return busiest;
 }
-#else /* CONFIG_SCHED_HMP */
+#else
+static inline struct rq *find_busiest_queue_hmp(struct lb_env *env,
+                                    struct sched_group *group)
+{
+	return NULL;
+}
+#endif
+
+/*
+ * find_busiest_queue - find the busiest runqueue among the cpus in group.
+ */
 static struct rq *find_busiest_queue(struct lb_env *env,
 				     struct sched_group *group)
 {
 	struct rq *busiest = NULL, *rq;
 	unsigned long busiest_load = 0, busiest_capacity = 1;
 	int i;
+
+	if (sysctl_sched_enable_hmp_task_placement)
+		return find_busiest_queue_hmp(env, group);
 
 	for_each_cpu_and(i, sched_group_cpus(group), env->cpus) {
 		unsigned long capacity, wl;
@@ -9334,7 +9355,6 @@ static struct rq *find_busiest_queue(struct lb_env *env,
 
 	return busiest;
 }
-#endif /* CONFIG_SCHED_HMP */
 
 /*
  * Max backoff if we encounter pinned tasks. Pretty arbitrary value, but
@@ -10361,8 +10381,7 @@ end:
 }
 
 #ifdef CONFIG_SCHED_HMP
-
-static inline int _nohz_kick_needed(struct rq *rq, int cpu, int *type)
+static inline int _nohz_kick_needed_hmp(struct rq *rq, int cpu, int *type)
 {
 	struct sched_domain *sd;
 	int i;
@@ -10396,12 +10415,19 @@ static inline int _nohz_kick_needed(struct rq *rq, int cpu, int *type)
 
 	return 0;
 }
-
-#else /* CONFIG_SCHED_HMP */
+#else
+static inline int _nohz_kick_needed_hmp(struct rq *rq, int cpu, int *type)
+{
+	return 0;
+}
+#endif
 
 static inline int _nohz_kick_needed(struct rq *rq, int cpu, int *type)
 {
 	unsigned long now = jiffies;
+
+	if (sysctl_sched_enable_hmp_task_placement)
+		return _nohz_kick_needed_hmp(rq, cpu, type);
 
 	/*
 	 * None are in tickless mode and hence no need for NOHZ idle load
@@ -10415,8 +10441,6 @@ static inline int _nohz_kick_needed(struct rq *rq, int cpu, int *type)
 
 	return (rq->nr_running >= 2);
 }
-
-#endif /* CONFIG_SCHED_HMP */
 
 /*
  * Current heuristic for kicking the idle load balancer in the presence
