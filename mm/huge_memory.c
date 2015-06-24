f16830a9e3aee62e55080b20dca52c327f298c73
Jiri Kosina <jkosina@suse.cz>
2015-06-24 16:56:07 -0700

    thp: cleanup how khugepaged enters freezer
    khugepaged_do_scan() checks in every iteration whether freezing(current)
    is true, and in such case breaks out of the loop, which causes
    try_to_freeze() to be called immediately afterwards in
    khugepaged_wait_work().
    
    If nothing else, this causes unnecessary freezing(current) test, and also
    makes the way khugepaged enters freezer a bit less obvious than necessary.
    
    Let's just try to freeze directly, instead of splitting it into two
    (directly adjacent) phases.
    
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -2831,7 +2831,7 @@ static void khugepaged_do_scan(void)
 
 		cond_resched();
 
-		if (unlikely(kthread_should_stop() || freezing(current)))
+		if (unlikely(kthread_should_stop() || try_to_freeze()))
 			break;
 
 		spin_lock(&khugepaged_mm_lock);
@@ -2852,8 +2852,6 @@ static void khugepaged_do_scan(void)
 
 static void khugepaged_wait_work(void)
 {
-	try_to_freeze();
-
 	if (khugepaged_has_work()) {
 		if (!khugepaged_scan_sleep_millisecs)
 			return;
