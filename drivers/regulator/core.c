dca3003d377e19b3710a3820db479017051ac240
Javier Martinez Canillas <javier@osg.samsung.com>
2015-07-15 16:10:29 +0200

    regulator: core: Fix memory leak in regulator_resolve_supply()
    The regulator_resolve_supply() function calls set_supply() which in turn
    calls create_regulator() to allocate a supply regulator.
    
    If an error occurs after set_supply() succeeded, the allocated regulator
    has to be freed before propagating the error code.
    
    Signed-off-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -118,6 +118,7 @@ static int _regulator_do_set_voltage(struct regulator_dev *rdev,
 static struct regulator *create_regulator(struct regulator_dev *rdev,
 					  struct device *dev,
 					  const char *supply_name);
+static void _regulator_put(struct regulator *regulator);
 
 static struct regulator_dev *dev_to_rdev(struct device *dev)
 {
@@ -1519,8 +1520,11 @@ static int regulator_resolve_supply(struct regulator_dev *rdev)
 	/* Cascade always-on state to supply */
 	if (_regulator_is_enabled(rdev)) {
 		ret = regulator_enable(rdev->supply);
-		if (ret < 0)
+		if (ret < 0) {
+			if (rdev->supply)
+				_regulator_put(rdev->supply);
 			return ret;
+		}
 	}
 
 	return 0;
