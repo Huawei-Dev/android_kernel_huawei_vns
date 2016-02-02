2e10f506754f0697db2f69315029df5a10167a12
Hemant Kumar <hemantk@codeaurora.org>
2016-02-01 18:56:43 -0800

    USB: Debug: Add centralized debug buffer for USB drivers
    Add a centralized debug buffer which can log events from all USB
    drivers in gadget directory. This will help in debugging issue
    from first instance rather than enabling dynamic debug log and
    reproducing again.
    
    Change-Id: Id0285d30bf640a63d904f1dd02dcd061f1819359
    Signed-off-by: Tarun Gupta <tarung@codeaurora.org>
    Signed-off-by: Azhar Shaikh <azhars@codeaurora.org>
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -8,6 +8,7 @@
 #include "configfs.h"
 #include "u_f.h"
 #include "u_os_desc.h"
+#include "debug.h"
 
 #include <chipset_common/hwusb/hw_usb_rwswitch.h>
 #include <chipset_common/hwusb/hw_usb_sync_host_time.h>
@@ -1886,6 +1887,8 @@ static int __init gadget_cfs_init(void)
 
 	config_group_init(&gadget_subsys.su_group);
 
+	debug_debugfs_init();
+
 	ret = configfs_register_subsystem(&gadget_subsys);
 
 #ifdef CONFIG_USB_CONFIGFS_UEVENT
@@ -1900,6 +1903,7 @@ module_init(gadget_cfs_init);
 
 static void __exit gadget_cfs_exit(void)
 {
+	debug_debugfs_exit();
 	configfs_unregister_subsystem(&gadget_subsys);
 #ifdef CONFIG_USB_CONFIGFS_UEVENT
 	if (!IS_ERR(android_class))
