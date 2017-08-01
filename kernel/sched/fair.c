f753ea1a2c44b630d822e00f111509683659a033
Brendan Jackman <brendan.jackman@arm.com>
2017-08-01 15:48:37 +0100

    UPSTREAM: sched/fair: Sync task util before slow-path wakeup
    We use task_util() in find_idlest_group() via capacity_spare_wake().
    This task_util() updated in wake_cap(). However wake_cap() is not the
    only reason for ending up in find_idlest_group() - we could have been sent
    there by wake_wide(). So explicitly sync the task util with prev_cpu
    when we are about to head to find_idlest_group().
    
    We could simply do this at the beginning of
    select_task_rq_fair() (i.e. irrespective of whether we're heading to
    select_idle_sibling() or find_idlest_group() & co), but I didn't want to
    slow down the select_idle_sibling() path more than necessary.
    
    Don't do this during fork balancing, we won't need the task_util and
    we'd just clobber the last_update_time, which is supposed to be 0.
    
    Change-Id: I935f4bfdfec3e8b914457aac3387ce264d5fd484
    Signed-off-by: Brendan Jackman <brendan.jackman@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Andres Oportus <andresoportus@google.com>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Josef Bacik <josef@toxicpanda.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Link: http://lkml.kernel.org/r/20170808095519.10077-1-brendan.jackman@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry-picked-from: commit ea16f0ea6c3d tip:sched/core)
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8078,7 +8078,7 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 		if (cpu != prev_cpu && wake_affine(affine_sd, p, prev_cpu, sync))
 			new_cpu = cpu;
 	}
-	
+
 	if (!sd) {
 		if (sd_flag & SD_BALANCE_WAKE) /* XXX always ? */
 			new_cpu = select_idle_sibling(p, prev_cpu, new_cpu);
